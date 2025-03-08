4c0ab8ba1ad4899a1537a4e7e296a1f0f27a176f
Victor Soto <vicsotojr@gmail.com>
2025-03-07 22:46:37 -0500

    Formatting code and removing unnecesary files

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'cbt-league-v5';  // Update cache version when changing files
+const CACHE_NAME = "cbt-league-v5"; // Update cache version when changing files
 const urlsToCache = [
-  '/index.html',
-  '/manifest.json',
-  '/assets/img/cbt-logo1.jpg',
-  '/assets/css/style.css',
+  "/index.html",
+  "/manifest.json",
+  "/assets/img/cbt-logo1.jpg",
+  "/assets/css/style.css",
 ];
 
 // Install service worker and cache assets
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   self.skipWaiting(); // Forces the new service worker to take over immediately
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
+      console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
 // Fetch resources: Try network first, fallback to cache
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request)
       .then((response) => {
@@ -32,7 +32,7 @@ self.addEventListener('fetch', (event) => {
 });
 
 // Activate service worker and remove old cache versions
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
