7f4d7cc26cfff87b60214f21624c95cd55504033
molinakSCSU <molinak4@southernct.edu>
2026-03-07 03:32:20 -0500

    fix

--- src/app/home-client.tsx
+++ src/app/home-client.tsx
@@ -152,7 +152,7 @@ export default function HomeClient() {
                 <div className="flex items-center gap-4 border-l-2 border-orange-500 pl-6">
                     <Trophy className="h-6 w-6 text-orange-500 shrink-0" />
                     <div>
-                        <p className="text-xs font-black uppercase tracking-[0.3em] text-zinc-500">Commissioner</p>
+                        <p className="text-xs font-black uppercase tracking-[0.4em] text-zinc-500">Commissioner</p>
                         <p className="text-lg font-black uppercase tracking-tight text-white">
                             Coach <span className="text-orange-500">Byrd</span>
                         </p>
