2f31d43b3f1c591d34026a1729185bd973e00f38
Victor Soto <vicsotojr@gmail.com>
2026-03-25 15:03:28 -0400

    Update stats via admin panel (3 / Game 3) + roster/media

--- src/lib/manual-player-overalls.ts
+++ src/lib/manual-player-overalls.ts
@@ -14,7 +14,7 @@ const SEASON_3_MANUAL_OVERALLS = {
   "colby winchester": 78,
   "darric myers": 86,
   "dashon smith": 90,
-  "david crenshaw": 90,
+  "david crenshaw": 89,
   "demetrius gordon": 80,
   "dlier mohammed": 86,
   "eric cummings": 82,
@@ -31,6 +31,7 @@ const SEASON_3_MANUAL_OVERALLS = {
   "jaylen crawford": 89,
   "jaylin davis": 78,
   "jermaine foster": 82,
+  "jonathan edwards": 90,
   "justin sheffield": 82,
   "kevin magliochetti": 83,
   "khalid moreland": 83,
@@ -38,7 +39,7 @@ const SEASON_3_MANUAL_OVERALLS = {
   "kuran iverson": 89,
   "kyle cookson": 83,
   "kyle federici": 85,
-  "lateef bilewu": 84,
+  "lateef bilewu": 86,
   "luke yoder": 83,
   "marcell robinson": 86,
   "markis christie": 78,
@@ -56,7 +57,7 @@ const SEASON_3_MANUAL_OVERALLS = {
   "roger hamelet": 80,
   "ronnie smith": 87,
   "ryan boehm": 80,
-  "sheriff bilewu": 86,
+  "sheriff bilewu": 87,
   "siah tait": 86,
   "tamar williams": 92,
   "tarice thompson": 81,
@@ -68,6 +69,7 @@ const SEASON_3_MANUAL_OVERALLS = {
   "tyshawn smith": 80,
   "will barton": 92,
   "zaire lott": 94,
+  "zion lott": 94,
 } as const satisfies Record<string, number>;
 
 const MANUAL_SEASON_OVERALLS = {
